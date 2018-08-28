b7831cfc4cf5708941d62786c0f875b835b5a849
Alezzo1592 <gonzalezalejandro1592@gmail.com>
2018-08-27 22:58:21 -0300

    add api structure

---
+++ grails-app/domain/users/User.groovy
@@ -0,0 +1,17 @@
+package users
+
+/**
+ * Created by agonzal on 8/27/18.
+ */
+class User implements Serializable {
+
+    Long id
+    String nickname
+    String firstName
+    String lastName
+    Integer age
+    String email
+    Date registrationDate
+
+
+}
